8fde7d6242060e2729b64e8f4833eb65fecdad0d
leoboyerbx <leoboyer.bx@gmail.com>
2018-11-20 10:00:36 +0100

    FInal verison 1 button quitter ne marche pas encore et il faut confirmer l'enregistrement mais sinon c'est cool

--- converter.py
+++ converter.py
@@ -20,7 +20,7 @@ import os
 
 
 ##------- Définition des fonctions --------##
-def open_file(path, callback):
+def open_file(path):
     global filename
     tmp = tempfile.mkdtemp()
     filename = os.path.splitext(os.path.basename(path))[0]
@@ -30,14 +30,15 @@ def open_file(path, callback):
         with gzip.open(path, 'rb') as f_in:
             with open(temp_fichier, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
-        return callback(etree.parse(temp_fichier))
+        return etree.parse(temp_fichier)
+    else: return None
 
-def convert_data(projectFile):
+def convert_data(projectFile, versionToConvert):
     global filename
     for project in (projectFile.xpath("/PremiereData/Project")):
         if project.get('Version'):
             print(project.get('Version'))
-            project.set('Version', '34')
+            project.set('Version', versionToConvert)
             print(project.get('Version'))
             return etree.tostring(projectFile, pretty_print=True)
 
@@ -50,37 +51,70 @@ def write_output_file(data):
     with gzip.open(output_file, 'wb') as f:
         f.write(data)
 
+
+def get_src_version(projectFile):
+    for project in (projectFile.xpath("/PremiereData/Project")):
+        if project.get('Version'):
+            return project.get('Version')
+
+def get_src_file():
+    global src_filename
+    global projectFile
+    src_filename = filedialog.askopenfilename(initialdir = "./",title = "Ouvrir un projet", filetypes = (("Projets Premiere Pro","*.prproj"),("Tous les fichiers","*.*")))
+    if src_filename is None:
+        return
+    entryTextfile.set(src_filename)
+    projectFile = open_file(src_filename)
+    src_version = get_src_version(projectFile)
+    fieldsrcversion.config(text=src_version)
+    entryTextDestV.set(int(src_version)-1)
+
+def convert():
+    version = destinationversion.get()
+    write_output_file(convert_data(projectFile, version))
+
 ##------- Variables globales --------##
-
-
 
 
 ##------- Création de la fenêtre -------##
 fen = Tk()
 fen.title('Premiere Converter')                # ---> On donne un titre à la fenêtre
-# # ---> La fenêtre fait 400*400px, et est située à 200px de la gauche de l'écran, à 100px du haut
-# fen.geometry('800x700+200+100')
-
-# ##-------- Création de la barre de menu ---------##
-# barre_menu = Menu(fen)
-# menu_fichier = Menu(barre_menu, tearoff=0)
-# menu_fichier.add_command(label="Ouvrir un niveau", command=ouvrir_niveau)
-# menu_fichier.add_command(label="Fermer le niveau", command=init_jeu, state='disabled')
-# menu_fichier.add_separator()
-# menu_fichier.add_command(label="Quitter", command=fen.quit)
-# barre_menu.add_cascade(label="Fichier", menu=menu_fichier)
-
-# fen.config(menu=barre_menu)
 
 # ##-------- Création des zones de texte ---------##
-# bienvenue = Label(fen, text='Bienvenue sur RushHour, déplacez les véhicules pour faire sortir la voiture rouge !')
-# bienvenue.pack()
+tmp = Label(fen, text='Convertir un projet Premiere Pro vers une ancienne verison')
+tmp.grid(row= 0, column = 0, columnspan=2,padx=5, pady=5)
 
+entryTextfile = StringVar()
+fieldname = Entry(fen, text="", width=50, state="readonly", textvariable=entryTextfile)
+entryTextfile.set('Ouvrir un fichier...')
+
+fieldname.grid(row= 1, column = 0,padx=5, pady=5)
+
+browse = Button(fen, text="Parcourir", command=get_src_file)
+browse.grid(row= 1, column = 1,padx=5, pady=5)
+
+tmp = Label(fen, text="Version du Projet source : ", justify="left")
+tmp.grid(row= 2, column = 0 ,padx=5, pady=5)
+fieldsrcversion = Label(fen, text="", width=3)
+fieldsrcversion.grid(row= 2, column = 1 ,padx=5, pady=5)
+
+tmp = Label(fen, text='Convertir vers: ', justify="right")
+tmp.grid(row= 3, column = 0,padx=5, pady=5)
+
+entryTextDestV= StringVar()
+destinationversion = Entry(fen, width=3, textvariable=entryTextDestV)
+destinationversion.grid(row= 3, column = 1 ,padx=5, pady=5)
+
+tmp = Button(fen, text="Quitter")
+tmp.grid(row=4, column=0, padx=5, pady=5)
+
+tmp = Button(fen, text="Convertir", command=convert)
+tmp.grid(row=4, column=1, padx=5, pady=5)
 
 
 ##------- Programme principal -------##
-converted_data = open_file(filedialog.askopenfilename(initialdir = "./",title = "Ouvrir un projet", filetypes = (("Projets Premiere Pro","*.prproj"),("Tous les fichiers","*.*"))), convert_data)
-write_output_file(converted_data)
+# converted_data = open_file(), convert_data)
+# write_output_file(converted_data)
 
-# fen.resizable(width=False, height=False) # on veut une fenêtre non redimensionnable
-# fen.mainloop()
+fen.resizable(width=False, height=False) # on veut une fenêtre non redimensionnable
+fen.mainloop()
